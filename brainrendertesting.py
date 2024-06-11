cd8b38c589eb2b540e23db3e73309ab496cdd9b8
Davis Reina-Guerra <105831652+davisreinaguerra@users.noreply.github.com>
2024-06-11 15:15:18 -0400

    Update brainrendertesting.py

--- brainrendertesting.py
+++ brainrendertesting.py
@@ -1,9 +1,10 @@
 # import the required packages
 import numpy as np
 import pandas as pd
-from brainrender import Scene, actors, cameras
-from brainrender.actors import Points
-from brainrender import settings
+import brainrender
+#from brainrender import Scene, actors, cameras
+#from brainrender.actors import Points
+#from brainrender import settings
 
 # function which converts a csv file to a numpy array 
 def csv_to_numpy_array(file_path):
@@ -14,19 +15,17 @@ def csv_to_numpy_array(file_path):
     return numpy_array
 
 #Set up brainrender scene
-settings.SHADER_STYLE = "cartoon"
+settings.SHADER_STYLE = "plastic"
 settings.BACKGROUND_COLOR = "white"
 settings.ROOT_ALPHA = 0.1
 settings.SHOW_AXES = False
 scene = Scene(atlas_name="allen_mouse_25um")
-STRd, GPi, GPe = scene.add_brain_region("STRd", "GPi", "GPe", alpha = 0.3)
-int = scene.add_brain_region("int", alpha = .5, color="white")
+GPi = scene.add_brain_region("GPi", alpha = 0.3)
 
-#PV positive TdTomato Negative
-file_path = 'some_cells.csv'  # Replace 'some_cells.csv' with the path to your CSV file if using your own data
-numpy_array = csv_to_numpy_array(file_path) * 1000
-cell_coordinates = np.array(pvpos_tdtomneg_numpy_array)
-cells = actors.Points(pvpos_tdtomneg_cell_coordinates,  name="", colors="magenta")
-scene.add(cells)
+#Place cells in position given their coordinates
+file_path = 'some_cells.csv'                                                          # Replace 'some_cells.csv' with the path to your CSV file if using your own data
+cell_coordinates = np.array(csv_to_numpy_array(file_path) * 1000)                     # brainrender expects nm, but Qupath gives mm, so multiply by 1000
+cells = actors.Points(\cell_coordinates, colors="magenta")                            # Create points actors with those coordinates
+scene.add(cells)                                                                      # Add th cells to the scene
 
-scene.render()
+scene.render()                                                                        # Render the scene 
